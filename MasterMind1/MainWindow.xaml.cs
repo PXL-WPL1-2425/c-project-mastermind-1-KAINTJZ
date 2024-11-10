f32b33108e46cd5fc8c7333c4a856cc2f65657ff
kaanopxl <12400198@student.pxl.be>
2024-11-10 19:18:31 +0100

    MasterMind-03  ComboBoxen  De 4 comboboxen worden gevuld met de 6 beschikbare kleuren.
    +
    
    Mijn eigen manier geimplementeert voorlopig voor de kleuren selector.

--- MasterMind1/MainWindow.xaml.cs
+++ MasterMind1/MainWindow.xaml.cs
@@ -8,8 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
-
+using System.Collections.Generic; 
 
 namespace MasterMind1
 {
@@ -21,11 +20,12 @@ namespace MasterMind1
         public MainWindow()
         {
             InitializeComponent();
-            GenerateRandomCode();
+            GenerateRandomKleur();
+            ComboBoxesKleurVuller();
         }
 
-        // Random kleur generator
-        private void GenerateRandomCode()
+        // Random kleur generator + om in de titel weer te geven
+        private void GenerateRandomKleur()
         {
             Random randomColorGenerator = new Random();
             secretCode = new List<string>();
@@ -34,28 +34,57 @@ namespace MasterMind1
             {
                 int index = randomColorGenerator.Next(availibleColors.Count);
                 secretCode.Add(availibleColors[index]);
-
             }
 
             this.Title = "Secret kleur combinatie is: " + string.Join(", ", secretCode);
-
         }
 
-       
+        // ComboBoxen vullen met beschikbare kleuren
+        private void ComboBoxesKleurVuller()
+        {
+            // Maak een lijst van ColorItem objecten met kleur en naam
+            var colors = new List<ColorItem>
+            {
+                new ColorItem { Name = "Rood", Color = Colors.Red },
+                new ColorItem { Name = "Geel", Color = Colors.Yellow },
+                new ColorItem { Name = "Oranje", Color = Colors.Orange },
+                new ColorItem { Name = "Wit", Color = Colors.White },
+                new ColorItem { Name = "Groen", Color = Colors.Green },
+                new ColorItem { Name = "Blauw", Color = Colors.Blue }
+            };
+
+            // Voeg de kleuren toe aan elke ComboBox
+            RandomColorComboBox1.ItemsSource = colors;
+            RandomColorComboBox2.ItemsSource = colors;
+            RandomColorComboBox3.ItemsSource = colors;
+            RandomColorComboBox4.ItemsSource = colors;
+        }
+
+        // Om kleur gegevens vast te stellen
+        public class ColorItem
+        {
+            public string Name { get; set; }
+            public Color Color { get; set; }
+        }
+
+  
 
 
-      
+        // Button functies: 
+
         // Kleuren checker
         private void Button_CheckColorCombination(object sender, RoutedEventArgs e)
         {
-
+            
         }
 
-        //Om applicatie te sluiten
+
+
+        // Om applicatie te sluiten
         private void Button_LeaveGame(object sender, RoutedEventArgs e)
         {
             Close();
         }
-
     }
 }
+
