0a990eb94ad5789d332549f4c68c84099f0dccf3
kaanopxl <12400198@student.pxl.be>
2024-11-10 18:45:43 +0100

    MasterMind-02  Code  Er wordt een random code van 4 kleuren gegenereerd. Om het spel makkelijker te testen wordt deze code in de titel van het window getoond. De 4 kleuren worden willekeurig gekozen uit een reeks van 6 beschikbare kleuren: rood, geel, oranje, wit, groen en blauw.
    +
    
    ComboBoxItems alvast verwijdert en voorbereiding op commit 3

--- MasterMind1/MainWindow.xaml.cs
+++ MasterMind1/MainWindow.xaml.cs
@@ -9,26 +9,53 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
+
 namespace MasterMind1
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private List<string> availibleColors = new List<string> { "Rood", "Geel", "Oranje", "Wit", "Groen", "Blauw" };
+        private List<string> secretCode;
+
         public MainWindow()
         {
             InitializeComponent();
+            GenerateRandomCode();
         }
 
+        // Random kleur generator
+        private void GenerateRandomCode()
+        {
+            Random randomColorGenerator = new Random();
+            secretCode = new List<string>();
+
+            for (int i = 0; i < 4; i++)
+            {
+                int index = randomColorGenerator.Next(availibleColors.Count);
+                secretCode.Add(availibleColors[index]);
+
+            }
+
+            this.Title = "Secret kleur combinatie is: " + string.Join(", ", secretCode);
+
+        }
+
+       
+
+
+      
+        // Kleuren checker
+        private void Button_CheckColorCombination(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        //Om applicatie te sluiten
         private void Button_LeaveGame(object sender, RoutedEventArgs e)
         {
             Close();
         }
 
-        private void Button_CheckColorCombination(object sender, RoutedEventArgs e)
-        {
-
-        }
     }
 }
